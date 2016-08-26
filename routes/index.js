5fa1719c7f66399b3f7915761d82ee4160c2ca45
Leonardy Kristianto <alendory@gmail.com>
2016-08-26 08:20:13 -0700

    moved some text to route variable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'EAGxOxford' });
+  res.render('index', {
+    title: 'EAGxOxford',
+    tagline: 'Doing Good Better',
+    conf_date: "18-20 November 2016",
+  });
 });
 
 module.exports = router;
